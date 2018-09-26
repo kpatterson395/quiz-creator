1ad8414900ed954214594d15ee55b8866480b158
Kristie Patterson <kpatterson395@gmail.com>
2018-09-26 12:33:28 -0400

    update routing

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const App = () => (
 
 
 ReactDOM.render(
-	<BrowserRouter>
+	<BrowserRouter basename={process.env.PUBLIC_URL || ''}>
 		<App />
 	</BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
